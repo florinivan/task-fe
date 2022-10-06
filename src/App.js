b2ca83ce3de2ed6413f9f37e8f26325e1b0ba676
Andrei Pop <pop.andrei@yahoo.com>
2022-10-06 13:10:06 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <section id="App-header">
+        <h1>Keep employees busy</h1>
+      </section>
+      <section id="Task-list">
+        This is where the tasks will be
+      </section>
+    </div>
+  );
+}
+
+export default App;
